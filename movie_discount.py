7f61d0f975ac1df3352f7d0dfd4b61035adb6591
gouravpandey009 <pandeygourav6592@gmail.com>
2026-09-04 11:12:12 +0530

    commit

---
+++ movie_discount.py
@@ -0,0 +1,9 @@
+# Take the customer's age.
+age = int(input("Enter your age: "))
+
+# Check whether the customer qualifies for the discount.
+if age >= 60:
+    print("Senior Citizen Discount Applied.")
+
+else:
+    print("Normal Ticket Price.")
